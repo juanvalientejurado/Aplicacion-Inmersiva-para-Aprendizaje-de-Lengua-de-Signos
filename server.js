5dc9e8d7cfd65d5608d31fbf6a395d72533aafa0
juanvalientejurado <juanvalientejurado@gmail.com>
2023-04-11 19:29:13 +0200

    Cambios en server y toma_datos. Ya funciona nodejs y las posiciones. Solo queda guardar en csv y grabar

--- server.js
+++ server.js
@@ -1,37 +1,48 @@
+const WebSocket = require('ws');
 const express = require('express');
-
-const fs = require('fs');
-const os = require('os');
-
 const app = express();
 const server = require("http").Server(app);
-const WebSocket = require("ws");
 const wss = new WebSocket.Server({ server });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+const path = require('path');
+const htmlFilePath = path.join(process.cwd(), 'index.html');
+const jsFilePath = path.join(process.cwd(), 'script_toma_datos.js');
 
-app.get('/', (req, res) => {
-  res.sendFile('public/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(htmlFilePath);
+  });
+
+app.get('/script_toma_datos.js', function(req, res) {
+    res.type('application/javascript');
+    res.sendFile(jsFilePath);
 });
+  
 
-wss.on("connection", function(ws){
-  let writeStream = null;
-  ws.on("message", function(message){
+
+wss.on("connection", function connection(ws){
+  console.log("Conexion hecha");
+
+  ws.on("message", function incoming(message){
     const msg = JSON.parse(message);
     
-    switch(msg.type){
+    switch(msg.msg){
       case "START":
-        writeStream = fs.createWriteStream(`data/${Date.now()}.json`);
+        //writeStream = fs.createWriteStream(`data/${Date.now()}.json`);
+        
         break;
       case "STOP":
-        if (writeStream) writeStream.end();
+        //if (writeStream) writeStream.end();
+        console.log("STOP");
         break;
       case "HAND":
-        writeStream.write(msg.payload + os.EOL);
+        //writeStream.write(msg.payload + os.EOL);
+        console.log("Waiting");
         break;
+      case "RECORDING":
+        console.log(msg.payload);
+        break;
+
       default:
     }
   });
