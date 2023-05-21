2d0ba2657d1688a9af4cd108590565898c5ae5cf
juanvalientejurado <juanvalientejurado@gmail.com>
2023-05-21 19:36:43 +0200

    Añadida clase none y normalizacionl. Queda añadir normalizacion en inferencia

--- script_red.js
+++ script_red.js
@@ -7,12 +7,13 @@ const clases = {
     vocale: 1,
     vocali: 2,
     vocalo: 3,
-    vocalu: 4
+    vocalu: 4,
+    none: 5
   };
 
 //Modelo(
 const INPUT_SHAPE = 150; //Tamaño del frame (n_coordenadas)
-const NUM_CLASSES = 5;
+const NUM_CLASSES = 6;
 
 
 
@@ -22,6 +23,7 @@ const vocale = JSON.parse(fs.readFileSync('.\\data\\vocalE.json', 'utf-8'));
 const vocali = JSON.parse(fs.readFileSync('.\\data\\vocalI.json', 'utf-8'));
 const vocalo = JSON.parse(fs.readFileSync('.\\data\\vocalO.json', 'utf-8'));
 const vocalu = JSON.parse(fs.readFileSync('.\\data\\vocalU.json', 'utf-8'));
+const none = JSON.parse(fs.readFileSync('.\\data\\sinvocal.json', 'utf-8'));
 
 //Variables que alojan los datos de cada archivo
 var datosA = []
@@ -29,8 +31,9 @@ var datosE =[]
 var datosI =[]
 var datosO=[]
 var datosU=[]
+var datosNone=[]
 
-for (let j = 0; j < 5; j++){
+for (let j = 0; j < 6; j++){
     //La funcion rellenar array crea un objeto con dos campos (frame: datos) y la clase a la que pertenece en funcion del archivo 
     datos = rellenarArray(j);
     switch (j){
@@ -39,11 +42,12 @@ for (let j = 0; j < 5; j++){
         case 2: datosI = datos; break;
         case 3: datosO = datos; break;
         case 4: datosU = datos; break;
+        case 5: datosNone = datos; break;
     }
 }
 var X_train, X_test, y_train, y_test;
 //Concatenamos los datos (arrays de objetos )
-const allData = datosA.concat(datosE, datosI, datosO, datosU);
+const allData = datosA.concat(datosE, datosI, datosO, datosU, datosNone);
 //Esta funcion devuelve los tensores
 var tensor_data = createDataTensor(allData);
 var X_train = tensor_data.X_train;
@@ -64,7 +68,7 @@ const predictions = model.predict(X_test);
 
 const y_test_pred = predictions.argMax(axis=1).arraySync();
 const confusionMatrix = tf.math.confusionMatrix(y_test, y_test_pred, NUM_CLASSES);
-//await model.save('file://./models/modelo_manos');
+await model.save('file://./models/modelo_normalizado');
 console.log(confusionMatrix.arraySync());
 }
 
@@ -96,6 +100,11 @@ function rellenarArray(index){
             file_length = vocalu.length; 
             vocal = vocalu;
             break;
+        case 5: 
+            file_length = none.length; 
+            vocal = none;
+            break;
+
         default:       
     }
 
@@ -120,14 +129,21 @@ function createDataTensor(data){
     for(let i = 0; i<data.length; i++){
         feature = data[i].frame;
         clase = data[i].clase;
-        coordenadas =[]
+        
+        coordenadas_posicion =[]
+        coordenadas_orientacion =[]
         for(let j = 0; j< feature.length; j++){
-            //Aqui es donde habría que meter la normalizacion para las posiciones
-            //Array de 150
-            coordenadas.push(feature[j].pos.x, feature[j].pos.y, feature[j].pos.z, 
-                feature[j].rot._x, feature[j].rot._y, feature[j].rot._z);
+            
+            //Array de 75. Los separamos para poder normalizarlos
+            coordenadas_posicion.push(feature[j].pos.x, feature[j].pos.y, feature[j].pos.z) 
+            //Array de 75
+            coordenadas_orientacion.push(feature[j].rot._x, feature[j].rot._y, feature[j].rot._z);
                 
         }
+        //Normalizamos las posiciones
+        var posicion_normalizada = normalize_positions(coordenadas_posicion);
+        //Una vez normalizados los comparamos
+        var coordenadas = posicion_normalizada.concat(coordenadas_orientacion);
         //Array de nframes filas por 150 columnas
         features.push(coordenadas);
         //Etiquetas
@@ -153,6 +169,97 @@ function createDataTensor(data){
     };
 }
 
+//Funcion para normalizar las posiciones
+function normalize_positions(coordenadas_posicion){
+    //Normalizaremos con respecto al centro de la mano y el tamaño de la manos
+    //El centro es el punto central entre un triángulo en 3D de la muñeca y la punta de los dedos indice y meñique
+    var wrist_x = coordenadas_posicion[0];
+    var wrist_y = coordenadas_posicion[1];
+    var wrist_z = coordenadas_posicion[2];
+    var indice_x = coordenadas_posicion[18];
+    var indice_y = coordenadas_posicion[19];
+    var indice_z = coordenadas_posicion[20];
+    var menique_x = coordenadas_posicion[72];
+    var menique_y = coordenadas_posicion[73];
+    var menique_z = coordenadas_posicion[74];
+
+    const hand_center = getCenterPoint(wrist_x, wrist_y, wrist_z, indice_x, indice_y, indice_z,
+        menique_x, menique_y, menique_z);
+    
+    //Calculamos el tamaño de la mano
+    const hand_size = getHandSize(coordenadas_posicion, hand_center);
+
+    //Los puntos se normalizan como =  (punto - centro)/tamaño
+    puntos_normalizados =[];//Este tiene 25 objetos
+    for (let i = 0; i < 25; i++){
+        var index = i*3;
+        var punto={
+            x: coordenadas_posicion[index],
+            y: coordenadas_posicion[index+1],
+            z: coordenadas_posicion[index+2]
+        }
+        puntos_normalizados.push(normalizePoints(punto, hand_center, hand_size));
+    }
+
+    coordenadas_posicion_normalizadas = [];
+    for(let i=0 ; i<puntos_normalizados.length; i++){
+        coordenadas_posicion_normalizadas.push(puntos_normalizados[i].x, puntos_normalizados[i].y,
+            puntos_normalizados[i].z);
+    }
+    
+    return coordenadas_posicion_normalizadas;
+}
+
+//Calculo del centro de la mano
+function getCenterPoint(wrist_x, wrist_y, wrist_z, indice_x, indice_y, indice_z, menique_x, menique_y, menique_z){
+    var center_x = (wrist_x + indice_x + menique_x)/3;
+    var center_y = (wrist_y + indice_y + menique_y)/3;
+    var center_z = (wrist_z + indice_z + menique_z)/3;
+
+    return {
+        x: center_x,
+        y: center_y,
+        z: center_z
+    }
+}
+
+//Calcula el tamaño de la mano
+function getHandSize(coordenadas_posicion, hand_center){
+    //El tamaño de la mano es el maximo de la distancia entre el centro de la mano  y el punto mas alejado
+    var distancias =[]
+    for (let i = 0; i < 25; i++){
+        var index = i*3;
+        var punto1={
+            x: coordenadas_posicion[index],
+            y: coordenadas_posicion[index+1],
+            z: coordenadas_posicion[index+2]
+        }
+        distancias.push(calculateDistance(punto1, hand_center));
+    }
+    var hand_size = Math.max(...distancias);
+
+    return hand_size;
+}
+
+//Funcion auxiliar que calcula la distancia de un punto a otro
+function calculateDistance(punto1, punto2){
+        const distanciaX = Math.abs(punto2.x - punto1.x);
+        const distanciaY = Math.abs(punto2.y - punto1.y);
+        const distanciaZ = Math.abs(punto2.z - punto1.z);
+      
+        const distancia = Math.sqrt(distanciaX ** 2 + distanciaY ** 2 + distanciaZ ** 2);
+        return distancia;
+}
+
+//Funcion que dado un punto, centro y distancia lo normlaiza
+function normalizePoints(punto, hand_center, hand_size){
+    return {
+        x: (punto.x - hand_center.x) / hand_size,
+        y: (punto.y - hand_center.y) / hand_size,
+        z: (punto.z - hand_center.z) / hand_size
+    }
+}
+
 //Creacion del modelo
 function createModel(){
     const model = tf.sequential();
@@ -164,6 +271,7 @@ function createModel(){
     return model;
 }
 
+//Entrenamiento del modelo
 async function trainModel(model, X_train, y_train){
     model.compile({
         optimizer: 'adam',
@@ -190,13 +298,7 @@ function aleatorizar(a,b){
     return Math.random() - 0.5;
 }
 
-//Funcion para guardar el modelo
-async function saveModel(model){
-    model.save('models/modelo_manos');
-}
-
 //Funcion auxiliar loss logaritmico
-//Progreso de la loss en logarítmico
 function logProgress(epoch, logs) {
     console.log(`Data for epoch ${epoch}, ${Math.sqrt(logs.loss)}`);
 }
--- utils/pruebajson.js
+++ utils/pruebajson.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const filename = 'vocalU.json';
+const filename = 'data\\sinvocal.json';
 const content = fs.readFileSync(filename, 'utf8');
 const data = JSON.parse(content);
 //Cada item del array es un frame
